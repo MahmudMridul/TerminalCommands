3807fd455915c5382cd44299d0d5e6d42bcac80a
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-03-14 11:14:52 +0600

    add: command to open pycharm project

--- PowerShellScripts/lc_solutions.ps1
+++ PowerShellScripts/lc_solutions.ps1
@@ -1,3 +1,4 @@
-start-process -FilePath "devenv" -ArgumentList "D:\LeetCodeSolutions\Csharp\Csharp.sln"
+# start-process -FilePath "devenv" -ArgumentList "D:\LeetCodeSolutions\Csharp\Csharp.sln"
+start-process -FilePath "pycharm64" -ArgumentList "D:\LeetCodeSolutions\Python"
 start-process -FilePath "wt" -ArgumentList "new-tab -d `"D:\LeetCodeSolutions`""
 start-process "chrome.exe" "https://leetcode.com"
