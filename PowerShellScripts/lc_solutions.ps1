da606cd1133e07a8fb23d1abba359416fda1d64b
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-03-18 09:35:24 +0600

    add: script to open multiple chrome tab

--- PowerShellScripts/lc_solutions.ps1
+++ PowerShellScripts/lc_solutions.ps1
@@ -1,4 +1,4 @@
 # start-process -FilePath "devenv" -ArgumentList "D:\LeetCodeSolutions\Csharp\Csharp.sln"
 start-process -FilePath "pycharm64" -ArgumentList "D:\LeetCodeSolutions\Python"
 start-process -FilePath "wt" -ArgumentList "new-tab -d `"D:\LeetCodeSolutions`""
-start-process "chrome.exe" "https://leetcode.com"
+start-process "chrome.exe" "https://leetcode.com https://github.com/MahmudMridul/LeetCodeSolutions/issues"
